e7a5cf46bec94dfbd4b29b204c4bd820201b9deb
Voykov Konstantin <koykov0@gmail.com>
2021-02-27 23:46:12 +0200

    Code comment issue.

--- bytealg.go
+++ bytealg.go
@@ -72,6 +72,9 @@ func trim(p, cut []byte, dir int) []byte {
 	return p[l : r+1]
 }
 
+// Split s to buf using sep as separator.
+//
+// This function if an alloc-free replacement of bytes.Split() function.
 func AppendSplit(buf [][]byte, s, sep []byte, n int) [][]byte {
 	if len(s) == 0 {
 		return buf
@@ -105,10 +108,18 @@ func IndexAt(p, sep []byte, at int) int {
 	return i + at
 }
 
+// ToUpper is an alloc-free replacement of bytes.ToUpper() function.
 func ToUpper(p []byte) []byte { return Map(unicode.ToUpper, p) }
+
+// ToLower is an alloc-free replacement of bytes.ToLower() function.
 func ToLower(p []byte) []byte { return Map(unicode.ToLower, p) }
+
+// ToTitle is an alloc-free replacement of bytes.ToTitle() function.
 func ToTitle(p []byte) []byte { return Map(unicode.ToTitle, p) }
 
+// Map returns modified p with all its characters modified according to the mapping function.
+//
+// See bytes.Map() function for details.
 func Map(mapping func(r rune) rune, p []byte) []byte {
 	maxbytes := len(p)
 	nbytes := 0
